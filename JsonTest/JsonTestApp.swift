0eda00fbca2f26b49892b1012a20363d222de319
Consultant <jaafarwerdi87@gmail.com>
2023-01-16 21:17:44 -0500

    first commit

---
+++ JsonTest/JsonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonTestApp.swift
+//  JsonTest
+//
+//  Created by Consultant on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct JsonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
